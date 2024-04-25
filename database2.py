6643b2225bb1655744734f79b4083d2f63d35aa5
82mahdi82 <141176041+82mahdi82@users.noreply.github.com>
2024-04-25 13:07:49 +0330

    yyyy

--- database2.py
+++ database2.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import time
 def create_database():
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost')
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db')
     cursor = cnx.cursor()
     cursor.execute("DROP database IF EXISTS data")
     cursor.execute("create database if not exists data")
@@ -17,7 +17,7 @@ def create_database():
     cnx.commit()
 
 def insert_users(cid,id,rem):
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost',database="data")
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from users where cid={cid}")  
     f = cursor.fetchall()
@@ -31,14 +31,14 @@ def insert_users(cid,id,rem):
     return "no"
 
 def insert_words(word):
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost',database="data")
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"insert into words (word) values ('{word}');")
     cursor.close()
     cnx.commit()
 
 def use_words():
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost',database="data")
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from words") 
     dict_product=cursor.fetchall()
@@ -48,7 +48,7 @@ def use_words():
 
 
 def use_users_id(id):
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost',database="data")
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from users where id='{id}'")  
     f = cursor.fetchall()
@@ -56,21 +56,21 @@ def use_users_id(id):
 
 
 def updete_users(cid,rem):
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost',database="data")
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"update users set rem={rem} where cid={cid}")
     cursor.close()
     cnx.commit()
 
 def delete_users(cid):
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost',database="data")
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"delete from users where cid={cid}") 
     cursor.close()
     cnx.commit()
 
 def use_users():
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost',database="data")
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from users") 
     dict_product=cursor.fetchall()
@@ -79,14 +79,14 @@ def use_users():
     return dict_product
 
 def insert_translations(text,language,language_target,mid):
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost',database="data")
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"insert into translations (text,language,language_target,mid) values ('{text}','{language}','{language_target}',{mid})")
     cursor.close()
     cnx.commit()
 
 def use_translations(text,language,language_target):
-    cnx = mysql.connector.connect(user='root', password='ma8h2dii',host='localhost',database="data")
+    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='myenux_db',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from translations where text='{text}' AND language='{language}' AND language_target='{language_target}'")   
     dict_product=cursor.fetchall()
