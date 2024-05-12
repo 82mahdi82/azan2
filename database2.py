a6956850e098cfc9c9f9a8f6ecea48dde41d4b0e
82mahdi82 <141176041+82mahdi82@users.noreply.github.com>
2024-05-12 16:40:31 +0330

    iuiui

--- database2.py
+++ database2.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import time
 def create_database():
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service')
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service')
     cursor = cnx.cursor()
     #cursor.execute("DROP database IF EXISTS data")
     cursor.execute("create database if not exists data")
@@ -24,13 +24,13 @@ def create_database():
     cnx.commit()
 
 def insert_product(photo_id,title,details,price):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"insert into product (photo_id,title,details,price) values ('{photo_id}','{title}','{details}',{price});")
     cursor.close()
     cnx.commit()
 def use_product():
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from product") 
     dict_product=cursor.fetchall()
@@ -38,7 +38,7 @@ def use_product():
     cnx.commit()
     return dict_product
 def use_product_id(id):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from product where id='{id}'")  
     f = cursor.fetchall()
@@ -57,7 +57,7 @@ def use_product_id(id):
 
 
 def insert_users(cid,id,rem):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from users where cid={cid}")  
     f = cursor.fetchall()
@@ -71,21 +71,21 @@ def insert_users(cid,id,rem):
     return "no"
 
 def use_users_cid(cid):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from users where cid='{cid}'")  
     f = cursor.fetchall()
     return f
 
 def insert_words(word):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"insert into words (word) values ('{word}');")
     cursor.close()
     cnx.commit()
 
 def use_words():
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from words") 
     dict_product=cursor.fetchall()
@@ -95,7 +95,7 @@ def use_words():
 
 
 def use_users_id(id):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from users where id='{id}'")  
     f = cursor.fetchall()
@@ -103,21 +103,21 @@ def use_users_id(id):
 
 
 def updete_users(cid,rem):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"update users set rem={rem} where cid={cid}")
     cursor.close()
     cnx.commit()
 
 def delete_users(cid):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"delete from users where cid={cid}") 
     cursor.close()
     cnx.commit()
 
 def use_users():
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from users") 
     dict_product=cursor.fetchall()
@@ -126,14 +126,14 @@ def use_users():
     return dict_product
 
 def insert_translations(text,language,language_target,mid):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"insert into translations (text,language,language_target,mid) values ('{text}','{language}','{language_target}',{mid})")
     cursor.close()
     cnx.commit()
 
 def use_translations(text,language,language_target):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from translations where text='{text}' AND language='{language}' AND language_target='{language_target}'")   
     dict_product=cursor.fetchall()
@@ -142,14 +142,14 @@ def use_translations(text,language,language_target):
     return dict_product
 
 def use_selse_list():
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"select * from sales")  
     f = cursor.fetchall()
     return f
 
 def insert_seles(cid,mid):
-    cnx = mysql.connector.connect(user='root', password='mDAg9x6Ictcx5MVLnuMf',host='mye-nye-service',database="data")
+    cnx = mysql.connector.connect(user='root', password='u3nZED2tr2ygrTAAHeib',host='data-txx-service',database="data")
     cursor = cnx.cursor(dictionary=True)
     cursor.execute(f"insert into sales (cid,mid) values ('{cid}',{mid});")
     cursor.close()
